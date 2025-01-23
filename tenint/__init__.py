0aaccf67c169c16ec41911f57020e186dfc95fa3
Steve McGrath <steve@mcgrath.sh>
2025-01-23 11:51:10 -0600

    Updated the connector console to always run 135 cols wide, and to improve debug reporting.

--- tenint/__init__.py
+++ tenint/__init__.py
@@ -2,4 +2,4 @@ from .models.credentials import Credential, TenableVMCredential, TenableSCCreden
 from .models.configuration import Configuration, Settings  # noqa F401
 from .connector import Connector  # noqa F401
 
-__version__ = '0.9.3'
+__version__ = '0.9.4'
